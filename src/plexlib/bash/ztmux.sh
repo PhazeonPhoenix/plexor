a75a3f43d76609e4f9deccc52708ae8ddd999ff4
PhazeonPhoenix <phoenix@phazeon.com>
2012-10-27 10:50:02 -0700

    Tmux status bar
    Based roughly on tmux-powerline. Launcher script now loads .conf files
    from plexlib/tmux and sources them; Simulating the conf.d style of
    configuration.

--- src/plexlib/bash/ztmux.sh
+++ src/plexlib/bash/ztmux.sh
@@ -1,3 +1,18 @@
 if [ $TERM != "screen-256color" ] && [  $TERM != "screen" ]; then
-    tmux attach || tmux new
+    if tmux has-session -t main 2>/dev/null; then
+        tmux attach -t main
+    else
+        tmux new -s main
+    fi
+fi
+
+
+if tmux has-session -t main 2>/dev/null; then
+    TMUXLIB=$HOME/.plexlib/tmux
+    if [ -d $TMUXLIB ]; then
+        for file in $(find $TMUXLIB -type f -name \*.conf); do
+            tmux source-file $file
+        done
+    fi
+    unset TMUXLIB
 fi
