9fb5281508d1ac536ea71a22597d075b987516d5
PhazeonPhoenix <phoenix@phazeon.com>
2013-01-31 04:18:00 -0800

    New tool, todo.sh-cli
    http://ginatrapani.github.com/todo.txt-cli/

--- install.sh
+++ install.sh
@@ -11,3 +11,11 @@ for file in $(find *); do
 done
 
 touch $HOME/.bashrc.local $HOME/.vimrc.local $HOME/.tmux.conf.local
+
+mkdir $HOME/bin > /dev/null 2>&1
+wget https://raw.github.com/ginatrapani/todo.txt-cli/master/todo.sh -O $HOME/bin/todo.sh
+chmod +x $HOME/bin/todo.sh
+mkdir $HOME/todo > /dev/null 2>&1
+
+wget https://raw.github.com/ginatrapani/todo.txt-cli/master/todo_completion -O $HOME/.bash_completion/todo_completion.sh
+
